0ded0c779dbf9583ffbe430f29beafe6730c6760
snandi <nandi@stat.wisc.edu>
2015-07-12 12:37:38 -0500

    Created RProject

--- RScript01_ReadData.R
+++ RScript01_ReadData.R
@@ -19,6 +19,34 @@ RDataPath <- '~/Stat/Stat_Competitions/Kaggle_Caterpillar_2015July/Data/'
 Today <- Sys.Date()
 
 Filename <- paste(RDataPath, 'tube.csv', sep='')
-Data <- read.csv(Filename, header=T)
-str(Data)
+tube <- read.csv(Filename, header=T)
+str(tube)
+
+Filename <- paste(RDataPath, 'train_set.csv', sep='')
+train_set <- read.csv(Filename, header=T)
+str(train_set)
+
+Filename <- paste(RDataPath, 'test_set.csv', sep='')
+test_set <- read.csv(Filename, header=T)
+str(test_set)
+
+Filename <- paste(RDataPath, 'bill_of_materials.csv', sep='')
+bill_of_materials <- read.csv(Filename, header=T)
+str(bill_of_materials)
+
+Filename <- paste(RDataPath, 'components.csv', sep='')
+components <- read.csv(Filename, header=T)
+str(components)
+
+Filename <- paste(RDataPath, 'comp_adaptor.csv', sep='')
+comp_adaptor <- read.csv(Filename, header=T)
+str(comp_adaptor)
+
+Filename <- paste(RDataPath, 'comp_boss.csv', sep='')
+comp_boss <- read.csv(Filename, header=T)
+str(comp_boss)
+
+Filename <- paste(RDataPath, 'comp_elbow.csv', sep='')
+comp_elbow <- read.csv(Filename, header=T)
+str(comp_elbow)
 
