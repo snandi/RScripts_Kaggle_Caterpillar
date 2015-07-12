b411571388b49e581589bb9008ee3146dae62dd2
snandi <nandi@stat.wisc.edu>
2015-07-12 17:30:12 -0500

    Added data extraction functions; prepared train data

---
+++ Prepare_train_set.R
@@ -0,0 +1,36 @@
+rm(list=ls(all.names=TRUE))
+rm(list=objects(all.names=TRUE))
+#dev.off()
+
+########################################################################
+## This script reads in all the data, using the functions
+########################################################################
+
+########################################################################
+## Run Path definition file                                           ##
+########################################################################
+RScriptPath <- '~/Stat/Stat_Competitions/Kaggle_Caterpillar_2015July/RScripts_Caterpillar/'
+Filename.Header <- paste(RScriptPath, 'HeaderFile_Caterpillar.R', sep='')
+source(Filename.Header)
+source(paste(RScriptPath, 'fn_Library_Caterpillar.R', sep=''))
+RPlotPath <- '~/Stat/Stat_Competitions/Kaggle_Caterpillar_2015July/Plots/'
+RDataPath <- '~/Stat/Stat_Competitions/Kaggle_Caterpillar_2015July/Data/'
+########################################################################
+Today <- Sys.Date()
+
+#fn_loadAllData(RDataPath=RDataPath)
+fn_loadData(RDataPath=RDataPath, File='train_set.csv')
+fn_loadData(RDataPath=RDataPath, File='tube.csv')
+
+dim(train_set)
+train_set$train_id <- row(train_set)
+train_tube_common <- intersect(names(train_set), names(tube))
+train_tube <- merge(x=train_set, y=tube, by=train_tube_common, all.x=T) 
+dim(train_tube)
+head(train_tube)
+
+tube_by_component_type <- fn_merge_tube_comp_type(File1='tube.csv', File2='bill_of_materials.csv')
+
+train_tube_comptype <- merge(x=train_tube, y=tube_by_component_type, by='tube_assembly_id',
+                             all.x=T, all.y=F)
+train_tube_comptype <- na.is.zero(train_tube_comptype)
--- fn_Library_Caterpillar.R
+++ fn_Library_Caterpillar.R
@@ -64,7 +64,7 @@ fn_loadAllData <- function(RDataPath){
   for(File in csvFiles){
     print(paste('Loading', File))
     Filename <- paste(RDataPath, File, sep='')
-    Data <- read.csv(Filename, header=T)
+    Data <- read.csv(Filename, header=T, quote='')
     File_trunc <- substr(x=File, start=1, stop=(nchar(File) - 4))
     assign(x=File_trunc, value=Data, envir=.GlobalEnv)
   }
@@ -73,10 +73,58 @@ fn_loadAllData <- function(RDataPath){
 ################################################################## 
 ## Load atasets individually, like test & train                 ##
 ##################################################################
-fn_loadData <- function(RDataPath, File='test_set.csv'){
+fn_loadData <- function(RDataPath, File='test_set.csv', Return=FALSE){
     print(paste('Loading', File))
     Filename <- paste(RDataPath, File, sep='')
-    Data <- read.csv(Filename, header=T)
+    Data <- read.csv(Filename, header=T, quote='')
     File_trunc <- substr(x=File, start=1, stop=(nchar(File) - 4))
     assign(x=File_trunc, value=Data, envir=.GlobalEnv)
+    if(Return) return(Data)
+}
+
+################################################################## 
+## Reshaped bill_of_materials, by component_type_id & their 
+## quantities, for each tube_assembly_id
+##################################################################
+fn_reshapeBOM <- function(File='bill_of_materials.csv'){
+  bom <- fn_loadData(RDataPath=RDataPath, File=File, Return=T)
+  
+  bom_long.c <- melt(data=bom, id.vars='tube_assembly_id', 
+                     measure.vars=c(grep(pattern='component', x=names(bom)))
+  )
+  bom_long.c <- na.omit(bom_long.c[order(bom_long.c$tube_assembly_id),])
+  names(bom_long.c) <- c('tube_assembly_id', 'component_num', 'component_id')
+  
+  bom_long.q <- melt(data=bom, id.vars='tube_assembly_id', 
+                     measure.vars=c(grep(pattern='quantity', x=names(bom)))
+  )
+  bom_long.q <- na.omit(bom_long.q[order(bom_long.q$tube_assembly_id),])
+  names(bom_long.q) <- c('tube_assembly_id', 'component_num', 'quantity')
+  bom_long.q$component_num <- gsub(pattern='quantity_', replacement='component_id_', 
+                                   x=bom_long.q$component_num)
+  
+  bom_long <- merge(x=bom_long.c, y=bom_long.q, by=intersect(names(bom_long.c), names(bom_long.q)), all=T)
+  bom_long <- na.omit(bom_long)  ## There are 8 components without any valid component_id
+  bom_long$component_num <- NULL
+  return(bom_long)
+}
+
+################################################################## 
+## Merge tube_assembly_id with the quantity of each component 
+##################################################################
+fn_merge_tube_comp_type <- function(File1='tube.csv', File2='bill_of_materials.csv'){
+  tube <- fn_loadData(RDataPath=RDataPath, File=File1, Return=T)
+  components <- fn_loadData(RDataPath=RDataPath, File='components.csv', Return=T)
+  bom_long <- fn_reshapeBOM(File=File2)
+  bom_comp <- merge(x=bom_long, y=components, by='component_id', all.x=T)
+  
+  component_type_qty <- aggregate(x=bom_comp$quantity, by=list(bom_comp$tube_assembly_id, bom_comp$component_type_id), 
+                                  FUN=sum)
+  names(component_type_qty) <- c('tube_assembly_id', 'component_type_id', 'comp_type_qty')
+  
+  tube_by_component_type <- reshape2::dcast(data=component_type_qty, 
+                                            formula=tube_assembly_id ~ component_type_id, 
+                                            value.var='comp_type_qty')
+  tube_by_component_type <- na.is.zero(X=tube_by_component_type)
+  return(tube_by_component_type)  
 }
