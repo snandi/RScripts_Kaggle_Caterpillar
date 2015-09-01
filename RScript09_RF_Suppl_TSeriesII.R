2d73668f9af020b75576e877b63cae0308c51d39
Song Wang <wangsong2012@gmail.com>
2015-08-31 20:22:58 -0500

    add factor variables to random Forest Model, getting the best result so far

---
+++ RScript09_RF_Suppl_TSeriesII.R
@@ -0,0 +1,137 @@
+
+rm(list=ls(all.names=TRUE))
+rm(list=objects(all.names=TRUE))
+#dev.off()
+
+########################################################################
+## This script got 0.2748 with Random Forest & log transformation, 
+## written by arnaud demytt on Kaggle
+########################################################################
+
+########################################################################
+## Run Path definition file                                           ##
+########################################################################
+RScriptPath <- '~/Stat/Stat_Competitions/Kaggle_Caterpillar_2015July/RScripts_Caterpillar/'
+Filename.Header <- paste(RScriptPath, 'HeaderFile_Caterpillar.R', sep='')
+source(Filename.Header)
+source(paste(RScriptPath, 'fn_Library_Caterpillar.R', sep=''))
+RPlotPath <- '~/Stat/Stat_Competitions/Kaggle_Caterpillar_2015July/Plots/'
+RDataPath <- '~/Stat/Stat_Competitions/Kaggle_Caterpillar_2015July/Data/'
+
+########################################################################
+Today <- Sys.Date()
+
+#fn_loadAllData(RDataPath=RDataPath)
+fn_loadData(RDataPath=RDataPath, File='train_set.csv')
+fn_loadData(RDataPath=RDataPath, File='test_set.csv')
+
+options(scipen = 10)
+
+###
+### Build train and test db
+###
+
+### Load train and test
+test = test_set
+train = train_set
+
+train$id = - (1:nrow(train))
+test$cost = 0
+
+train = rbind(train, test)
+
+### Merge datasets if only 1 variable in common
+continueLoop = TRUE
+while(continueLoop){
+  continueLoop = FALSE
+  for(f in dir(RDataPath)){
+    print(f)
+    filename <- paste0(RDataPath, f)
+    if(substrRight(filename, 3) != 'csv') next      ## Because the folder contains file_descriptions
+    d = read.csv(filename)
+    commonVariables = intersect(names(train), names(d))
+    if(length(commonVariables) == 1){
+      train = merge(train, d, by = commonVariables, all.x = TRUE)
+      continueLoop = TRUE
+      print(dim(train))
+    }
+  }
+}
+#compents.csv something wierd, deleted one row
+
+#   f =  dir(RDataPath)[i]
+#   d =read.csv(paste0(RDataPath,f))
+#   i=i+1
+
+train$quote_date <- as.Date(train$quote_date)
+
+### Clean NA values
+for(i in 1:ncol(train)){
+  if(is.numeric(train[,i])){
+    train[is.na(train[,i]),i] = -1
+  }
+  if(is.factor(train[,i])){
+    train[,i] = as.character(train[,i])
+    train[is.na(train[,i]),i] = "NAvalue"
+    train[,i] = as.factor(train[,i])
+  }
+}
+
+
+### Clean variables with too many categories
+
+for(i in 1:ncol(train)){
+  if(is.factor(train[,i])){
+    freq = data.frame(table(train[,i]))
+    freq = freq[order(freq$Freq, decreasing = TRUE),]
+    train[,i] = as.character(match(train[,i], freq$Var1[1:30]))
+    train[is.na(train[,i]),i] = "rareValue"
+    train[,i] = as.factor(train[,i])
+  }
+}
+
+train$Year <- format(as.yearqtr(train$quote_date), '%Y')
+for( i in 1:length(train$Year) ){
+  if(! train$Year[i]%in% c("2011","2012","2013","2014")){
+    train$Year[i]="other"
+  }
+}
+train$Year <- as.factor(train$Year)
+#table(train$Year)
+train$Qtr <- format(as.yearqtr(train$quote_date), '%q')
+train$Qtr <- as.factor(train$Qtr)
+
+
+test = train[which(train$id > 0),]
+train = train[which(train$id < 0),]
+
+###
+### Evaluate RF predictions by splitting the train db in 80%/20%
+###
+
+# dtrain_cv = train[which(train$id %% 5 > 0),]
+# dtest_cv = train[which(train$id %% 5 == 0),]
+# 
+# ### Train randomForest on dtrain_cv and evaluate predictions on dtest_cv
+# set.seed(123)
+# rf1 = randomForest(dtrain_cv$cost~., dtrain_cv[,-match(c("id", "cost"), names(dtrain_cv))], ntree = 10, do.trace = 2)
+# 
+# pred = predict(rf1, dtest_cv)
+# sqrt(mean((log(dtest_cv$cost + 1) - log(pred + 1))^2)) # 0.2589951
+# 
+# ### With log transformation trick
+# set.seed(123)
+# rf2 = randomForest(log(dtrain_cv$cost + 1)~., dtrain_cv[,-match(c("id", "cost"), names(dtrain_cv))], ntree = 10, do.trace = 2)
+# pred = exp(predict(rf2, dtest_cv)) - 1
+# 
+# sqrt(mean((log(dtest_cv$cost + 1) - log(pred + 1))^2)) # 0.2410004
+
+### Train randomForest on the whole training set
+rf = randomForest(log(train$cost + 1)~., train[,-match(c("id", "cost"), names(train))], ntree = 20, do.trace = 2)
+
+pred = exp(predict(rf, test)) - 1
+
+submitDb = data.frame(id = test$id, cost = pred)
+submitDb = aggregate(data.frame(cost = submitDb$cost), by = list(id = submitDb$id), mean)
+
+write.csv(submitDb, "submit1.csv", row.names = FALSE, quote = FALSE)
